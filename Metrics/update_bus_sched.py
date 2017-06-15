bdd3d79d82f58c226c5ea97d7b03dd7d8d97e3b0
Shay <lehmann.shay@gmail.com>
2017-06-14 20:57:11 -0400

    fixed some errors in schedule

--- Metrics/update_bus_sched.py
+++ Metrics/update_bus_sched.py
@@ -6,11 +6,11 @@ import zipfile as zf
 import StringIO
 from datetime import datetime
 from datetime import timedelta
+import numpy as np
 import re
 import json
-datadirectory = 'C:\Users\Nurvirta\OneDrive\CUSP\Capstone'
+datadirectory = '/Users/shay/CUSP/transitcenter_capstone/data/'
 
-a = datetime.now()
 def getSched():
     bx = 'http://web.mta.info/developers/data/nyct/bus/google_transit_bronx.zip'
     bk = 'http://web.mta.info/developers/data/nyct/bus/google_transit_brooklyn.zip'
@@ -26,21 +26,17 @@ def getSched():
             name = re.search(r'transit_(.*?).zip', boro).group(1)
         else:
             name = 'busco'
-        print "Downloading data for boro %s..." %boro
         r = requests.get(boro, stream = True)
         z = zf.ZipFile(StringIO.StringIO(r.content))
-        print "Extracting data for boro %s..." %boro
         z.extractall(datadirectory + '/schedules/' + name)
         temp = pd.read_csv(datadirectory + '/schedules/' + name + '/stop_times.txt')
         trips = pd.read_csv(datadirectory + '/schedules/' + name + '/trips.txt')
         cal = pd.read_csv(datadirectory + '/schedules/' + name + '/calendar.txt')
         enddate[name] = max(cal.end_date)
-        print "Merging data..."
         temp = temp.merge(trips[['route_id', 'trip_id', 'direction_id', 'service_id']], how = 'left')
         temp = temp.merge(cal)
         sched = sched.append(temp)
     sched['rds_index'] = sched['route_id'] + sched['direction_id'].astype(str) + sched['stop_id'].astype(str)
-    print "Finish downloading data."
     return sched, enddate
 
 scheds, enddate = getSched()
@@ -55,10 +51,9 @@ daysofweek_loop.insert(0, 'sunday')
 daysofweek_loop.insert(0, 'arrival_time')
 
 # does anyone know a faster way to make this happen?
-print "Get days of week..."
 scheds[daysofweek] = scheds[daysofweek].apply(lambda x: list(x[daysofweek_loop]) if
                         x.arrival_time > '23:59:59' else x, axis = 1)
-print "Convert arrival time..."
+
 scheds.arrival_time = scheds.arrival_time.apply(lambda x: datetime.strptime(x, '%H:%M:%S') if int(x[:2]) < 24
                     else datetime.strptime(x.replace(x[:2], str(int(x[:2])-24).zfill(2), 1), '%H:%M:%S'))
 
@@ -75,19 +70,19 @@ sun = scheds[scheds.sunday == 1].sort_values(['rds_index', 'arrival_time'])
 # if first item in rds, get last arrivaltime from previous day
 # NOTE TO SELF: DEAL WITH SCHEDULE CHANGE DAY
 # NOTE TO SELF: DEAL WITH HANDFUL OF STOPS THAT HAVE NO HEADWAY
-print "Calculate headways..."
 dfs = [mon, tues_to_thurs, fri, sat, sun]
 for i, df in enumerate(dfs):
     # calc time delta between each row
     df['headways'] = df.arrival_time.diff()
-    df['headways'] = df['headways'].apply(lambda x: x.seconds / 60.0)
+
     if i < 4:
         df2 = dfs[i + 1]
     else:
         df2 = dfs[0]
     # deal with rds_index change by looking at last arrival of previous day
     # assign the difference between those two times + 24 hours since the script
-    # only recognizes one day 
+    # only recognizes one day
+    df['headways'] = df['headways'].apply(lambda x: x.seconds / 60.0)
     mask = df['rds_index'] != df['rds_index'].shift(1)
     masked = df[mask].merge(df2.groupby('rds_index', as_index = False)\
             .max()[['rds_index', 'arrival_time']], on='rds_index', how = 'left')
@@ -95,17 +90,15 @@ for i, df in enumerate(dfs):
             for i in masked.index]
     diff = [x.seconds / 60.0 for x in diff]
     df.loc[mask, 'headways'] = diff
-    df.drop(['start_date', 'end_date', 'departure_time'], axis = 1, inplace = True)
+    df.drop(['departure_time'], axis = 1, inplace = True)
     df.drop(daysofweek[1:], axis = 1, inplace = True)
 
-print "Exporting data..."
-mon.to_csv('schedules/monday.csv')
-tues_to_thurs.to_csv('schedules/tues_to_thurs.csv')
-fri.to_csv('schedules/fri.csv')
-sat.to_csv('schedules/sat.csv')
-sun.to_csv('schedules/sun.csv')
+
+mon.to_csv('schedules/monday.csv', index = False)
+tues_to_thurs.to_csv('schedules/tues_to_thurs.csv', index = False)
+fri.to_csv('schedules/fri.csv', index = False)
+sat.to_csv('schedules/sat.csv', index = False)
+sun.to_csv('schedules/sun.csv', index = False)
+
 
 json.dump(enddate, open('schedules/enddate.txt', 'w+'))
-b = datetime.now()
-print "DONE!"
-print b-a
